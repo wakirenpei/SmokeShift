496082b12aa4bc20fdb9476426c4e2e9ccdb4c51
脇廉平 <renpei@MacBook-Air.local>
2024-08-09 00:55:04 +0900

    Rilsの設定周り

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source "https://rubygems.org"
 
 ruby "3.2.3"
 
+# 日本語対応
+gem 'rails-i18n'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.4"
 
--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,17 @@ module Myapp
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+
+    config.time_zone = 'Tokyo'   # タイムゾーンを日本時間に設定
+    config.active_record.default_timezone = :local   # DBのタイムゾーンをローカルタイムに設定
+
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.helper false             # helper ファイルを作成しない
+      g.test_framework false     # test ファイルを作成しない
+      g.skip_routes true         # ルーティングの記述を作成しない
+    end
+    config.i18n.default_locale = :ja  # デフォルトのロケールを日本語(:ja)に設定
+    config.i18n.load_path+=Dir[Rails.root.join('config','locales','**','*.yml').to_s]  # 訳文ファイルの読み込み先を追加
   end
 end
