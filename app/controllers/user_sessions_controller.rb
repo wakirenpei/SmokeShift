77c6d419e4a15c607333affe06a02164dcc60d0c
脇廉平 <renpei@MacBook-Air.local>
2024-08-23 22:40:33 +0900

    ログイン処理

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,23 @@
+class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
+  def new
+    @user = login(params[:email], params[:password])
+  end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_to root_path, notice: 'ログインしました。'
+    else
+      flash.now[:alert] = 'ログインに失敗しました'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, status: :see_other, notice: 'ログアウトしました。'
+  end
+end
