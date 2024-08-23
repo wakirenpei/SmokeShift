8b354ea3eaf003041a503d85749a9c6271adfbbe
脇廉平 <renpei@MacBook-Air.local>
2024-08-23 20:51:44 +0900

    フラッシュメッセージの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # フラッシュメッセー時の色を指定
+  def flash_background_color(type)
+    case type.to_sym
+    when :notice then 'bg-customForm'
+    when :alert then 'bg-red-500'
+    when :error then 'bg-red-500'
+    else 'bg-customForm'
+    end
+  end
 end
