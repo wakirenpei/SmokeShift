258907f06f6f3b044efaae51aa6c966792c5f781
脇廉平 <renpei@MacBook-Air.local>
2024-09-01 21:06:13 +0900

    タバコ登録機能作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,13 @@ module.exports = {
         borderColor: 'rgba(229, 231, 235, 0.2)', // ボーダー色
         borderColor2: 'rgba(229, 231, 235, 0.1)', // ボーダー色
         customGreen: '#78a22a', // カスタムグリーン
+        customGreenHover: '#5c7a20', // カスタムグリーンのhover時の色
         customForm: '#232323', // フォームのテキスト欄の背景色
         labelColor: '#D4D4D4', // ラベルの文字色
+        secondaryButton: '#4A5568', // セカンダリボタンの背景色
+        secondaryButtonHover: '#2D3748',
+        cardBackground: '#2a2a2a', // カードの背景色
+        itemBackground: '#333333', // アイテムの背景色
       },
     },
   },
