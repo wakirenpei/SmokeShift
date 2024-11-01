2169d07b999c629396fcb6e6adfe99eb5429acd0
脇廉平 <renpei@MacBook-Air.local>
2024-11-01 23:52:25 +0900

    パスワードリセット事前準備

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_30_124459) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_01_143617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,7 +87,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_30_124459) do
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.integer "smoking_status", default: 0, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   add_foreign_key "cigarettes", "users"
